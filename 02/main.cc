00b896693b71913279ba3bc9f128907912fc2f9b
Fabrice Leal <fabrice.leal.ch@gmail.com>
2013-01-19 04:20:59 -0800

    compiling!

--- 02/main.cc
+++ 02/main.cc
@@ -28,7 +28,7 @@ using namespace llvm;
 using namespace std;
 
 Module* makeLLVMModule() {
-	Module* mod = new Module("tut2");
+	Module* mod = new Module("tut2", getGlobalContext());
 	
 	Constant* c = mod->getOrInsertFunction(
 																				 "gcd",
@@ -38,6 +38,8 @@ Module* makeLLVMModule() {
 																				 NULL);
 	Function* gcd = cast<Function>(c);
 
+	gcd->setCallingConv(CallingConv::C);
+
 	Function::arg_iterator args = gcd->arg_begin();
 	Value* x = args++;
 	x->setName("x");
@@ -46,11 +48,11 @@ Module* makeLLVMModule() {
 
 	// This example has branching, so we create a block for 
 	// each "section" of code
-	BasicBlock* entry = BasicBlock::Create("entry", gcd);
-	BasicBlock* ret = BasicBlock::Create("return", gcd);
-	BasicBlock* cond_false = BasicBlock::Create("cond_false", gcd);
-	BasicBlock* cond_true = BasicBlock::Create("cond_true", gcd);
-	BasicBlock* cond_false_2 = BasicBlock::Create("cond_false_2", gcd);
+	BasicBlock* entry = BasicBlock::Create(getGlobalContext(), "entry", gcd);
+	BasicBlock* ret = BasicBlock::Create(getGlobalContext(), "return", gcd);
+	BasicBlock* cond_false = BasicBlock::Create(getGlobalContext(), "cond_false", gcd);
+	BasicBlock* cond_true = BasicBlock::Create(getGlobalContext(), "cond_true", gcd);
+	BasicBlock* cond_false_2 = BasicBlock::Create(getGlobalContext(), "cond_false_2", gcd);
 
 	// ** entry **
 	IRBuilder<> builder(entry);
